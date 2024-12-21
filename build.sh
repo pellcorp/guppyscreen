4d77d274f2ebcf69f953bb2869f7204cab81b960
Jason Pell <jason@pellcorp.com>
2024-12-21 15:33:48 +1100

    cleanup some build scripts and docs

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CROSS_COMPILE_ARG=""
+if [ "$CROSS_COMPILE" != "SIMULATION" ]; then
+    CROSS_COMPILE_ARG="CROSS_COMPILE=mipsel-buildroot-linux-musl-"
+fi
+
+# this line deletes just the guppyscreen objects so they are all rebuilt
+docker run -ti -v $PWD:$PWD pellcorp/guppydev /bin/bash -c "cd $PWD && $CROSS_COMPILE_ARG rm build/obj/src/*"
+docker run -ti -v $PWD:$PWD pellcorp/guppydev /bin/bash -c "cd $PWD && $CROSS_COMPILE_ARG make $@"
