c51d0b9df6173dc2e5c112f20a047e9e701a6bf9
Jason Pell <jason@pellcorp.com>
2025-03-29 09:55:46 +1100

    home all with just G28 not G28 X Y Z

--- release.sh
+++ release.sh
@@ -14,6 +14,8 @@ cp -r ./themes $RELEASES_DIR
 cp guppyscreen.json $RELEASES_DIR
 echo "GIT_SHA=$GIT_SHA" > $RELEASES_DIR/release.info
 echo "GIT_BRANCH=$GIT_BRANCH" >> $RELEASES_DIR/release.info
+echo "TIMESTAMP=$(date +%s)" >> $RELEASES_DIR/release.info
+
 if [ "$ASSET_NAME" = "guppyscreen-smallscreen" ]; then
   sed -i 's/"display_rotate": 3/"display_rotate": 2/g' $RELEASES_DIR/guppyscreen.json
 elif [ "$ASSET_NAME" = "guppyscreen-rpi" ]; then
